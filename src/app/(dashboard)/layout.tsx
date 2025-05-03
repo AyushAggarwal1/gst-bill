4b3e2770af3a1ffd4b4825c5b062d81924c90dd1
Ayush Aggarwal <ayushaggarwal1136@gmail.com>
2025-05-04 00:52:50 +0530

    Revert "feat: logger init (#6)" (#9)
    This reverts commit 5870a1901ef4851001a208db984b071beae8f95a.

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -27,7 +27,6 @@ export default function DashboardLayout({
     { name: "Items", href: "/dashboard/items" },
     { name: "Bills", href: "/dashboard/bills" },
     { name: "Profile", href: "/dashboard/profile" },
-    { name: "Logs", href: "/dashboard/logs" },
   ];
 
   const isActive = (path: string) => {
