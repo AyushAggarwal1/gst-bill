2059387ab16569875218d559aab6e0d2b8654269
Ayush Aggarwal <ayushaggarwal1136@gmail.com>
2025-05-04 00:52:19 +0530

    Revert "feat: logger v2 (#7)" (#8)
    This reverts commit 4df3d3f731059356e05b2584e6e2fe6ffa9467f5.

--- next.config.js
+++ next.config.js
@@ -1,34 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  /* Config options for production */
-  output: 'standalone', // Optimized for hosting platforms like Render
-  poweredByHeader: false, // Remove the `X-Powered-By: Next.js` header
-  reactStrictMode: true,
-  swcMinify: true,
-  
-  // Configure headers if needed
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-        ],
-      },
-    ];
-  },
+  /* config options here */
 };
 
 module.exports = nextConfig; 
