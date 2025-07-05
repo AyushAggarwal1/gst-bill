3c67d86fe7da69b50d077b4199afba44d4e065c2
AyushAggarwal1 <ayushaggarwal1136@gmail.com>
2025-07-05 21:17:07 +0530

    fix: db backup workflow

--- db-backup.sh
+++ db-backup.sh
@@ -11,8 +11,8 @@ BRANCH="main"  # or a separate branch for generated files
 git clone --depth=1 --branch=$BRANCH $REPO_URL db-repo
 
 # Run your file generation script
-pip install -r ../usefulScripts/exportSupabaseDbPy/requirements.txt
-python3 ../usefulScripts/exportSupabaseDbPy/egressSupdabaseDb.py  # Generates output.json for example
+pip install -r ./usefulScripts/exportSupabaseDbPy/requirements.txt
+python3 ./usefulScripts/exportSupabaseDbPy/egressSupdabaseDb.py  # Generates output.json for example
 
 # Copy generated file
 cd usefulScripts/exportSupabaseDbPy
