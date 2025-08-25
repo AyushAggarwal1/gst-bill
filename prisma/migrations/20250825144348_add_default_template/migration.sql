76efdb909ff01b94c619cff320a46fb78db9a3b4
AyushAggarwal1 <ayushaggarwal1136@gmail.com>
2025-08-25 15:01:47 +0000

    removed profilePhoto migration

--- prisma/migrations/20250825144348_add_default_template/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `profilePhoto` on the `Profile` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."Profile" DROP COLUMN "profilePhoto";
